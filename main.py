8df5993da89e9be2625dd200bb7ab75a3e9de837
𝕭. <iambhupendra69@gmail.com>
2026-09-11 15:00:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ shift = int(input("Enter a random number(0-9): "))
 def caesar(message, offset):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
+
     for char in message.lower():
         if char == ' ':
             encrypted_text += char
@@ -11,6 +12,7 @@ def caesar(message, offset):
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
+
     print('plain text:', message)
     print('encrypted text:', encrypted_text)
 
